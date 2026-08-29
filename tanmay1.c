d7a6f3a79a2fefdf90894ec2e21c884d2ce3ec32
tanmaytakale575-bit <tanmaytakale575@gmail.com>
2026-08-29 15:17:58 +0530

    Add files via upload

---
+++ tanmay1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum= 55;
+    per=88.88;
+    grade= 'a';
+
+    printf("---student imformation---/n");
+    printf("\n roll number : %d",rollnum);
+    printf("\n percentage:%f",per);
+    printf("\n grade : %c",grade);
+
+    return 0;
+}
