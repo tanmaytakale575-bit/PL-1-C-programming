6d11b3beb2c3e58bdb234b9bce4b3dc94642a196
tanmaytakale575-bit <tanmaytakale575@gmail.com>
2026-08-29 15:36:49 +0530

    Add files via upload

---
+++ tanmay2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int rollnum;
+    float per;
+    char grade;
+
+    printf("\n enter student rollnumber:");
+    scanf("%d",&rollnum);
+    printf("\n enter student percentage:");
+    scanf("%f",&per);
+    printf("\n enter student grade :");
+    scanf(" %c",&grade);
+
+    return 0;
+
+}
