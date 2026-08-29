ca3461d7385e42b7ab0908d58227cef3749839c2
tanmaytakale575-bit <tanmaytakale575@gmail.com>
2026-08-29 16:19:15 +0530

    Add files via upload

---
+++ tanmay5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 33;
+    int b = 47;
+    int c = 20;
+    float average;
+
+    average = (a + b + c)/3;
+    printf("\n avrage = %f" , average);
+
+    return 0;
+
+}
