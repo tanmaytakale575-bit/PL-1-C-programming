e3e2430cd68b8bf22f982eff19e410ddde3f07cb
tanmaytakale575-bit <tanmaytakale575@gmail.com>
2026-08-29 16:12:26 +0530

    Add files via upload

---
+++ tanmay4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float radius, area;
+
+    printf("\n enter the radius of the circle:");
+    scanf("%f",&radius);
+    area=(3.14*radius*radius);
+    printf("\n area of circle= %f",area);
+
+    return 0;
+
+}
